7e6480ad3214c07129e1a3dee9c06b5990235dc9
Mudit Choudhary <cmudit60@gmail.com>
2024-07-25 12:53:27 +0530

    Make conversation routes and controllers

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -1,5 +1,129 @@
+import ConversationModel from "../models/Conversation.model";
+
 export async function addConversationMessage(req, res) {
   try {
+    const { senderId, receiverId, author, content } = req.body;
+    const conversationExist = await ConversationModel.findOne({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+    if (conversationExist) {
+      const updatedConversation = await ConversationModel.findByIdAndUpdate(
+        conversationExist._id,
+        {
+          $push: {
+            messages: {
+              author_id: senderId,
+              author: author,
+              content: content,
+            },
+          },
+        },
+        { new: true }
+      );
+      return res.status(200).json({ updatedConversation });
+    }
+    if (!conversationExist) {
+      const newConversation = await ConversationModel.create({
+        senderId,
+        receiverId,
+        messages: [
+          {
+            author_id: senderId,
+            author: author,
+            content,
+          },
+        ],
+      });
+      return res.status(200).json({ newConversation });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function getSingleConversation(req, res) {
+  try {
+    const { senderId, receiverId } = req.body;
+    const conversation = await ConversationModel.findOne({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+    return res.status(200).json({ updatedConversation });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function deleteMessageFromConversation(req, res) {
+  try {
+    const { senderId, receiverId, messageId } = req.body;
+    const conversation = await ConversationModel.findOne({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+    await ConversationModel.findByIdAndUpdate(
+      conversation._id,
+      {
+        $pull: {
+          messages: {
+            _id: messageId,
+          },
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json({ updatedConversation });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+let doctorResult = await DoctorModel.updateOne(
+  { _id: doctor._id, "appointments.commonId": commonId },
+  {
+    $set: {
+      "appointments.$.appointmentAt": appointmentAt,
+      "appointments.$.appointmentOn": appointmentOn,
+    },
+  }
+);
+export async function editMessageFromConversation(req, res) {
+  try {
+    const { senderId, receiverId, messageId, content } = req.body;
+    const conversation = await ConversationModel.findOne({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+    await ConversationModel.updateOne(
+      { _id: conversation._id, "messages._id": messageId },
+      {
+        $set: {
+          "messages.$.content": {
+            content,
+          },
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json({ updatedConversation });
   } catch (error) {
     console.error(error);
     res.status(500).json({
--- routes/conversation.routes.js
+++ routes/conversation.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { addConversationMessage } from "../controllers/conversation.controller.js";
+import {
+  addConversationMessage,
+  getSingleConversation,
+  deleteMessageFromConversation,
+  editMessageFromConversation,
+} from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
-// add message
-router.post("/message", addConversationMessage);
+router.post("/new", addConversationMessage);
+router.get("/get-conversation", getSingleConversation);
+router.delete("/delete-message", deleteMessageFromConversation);
+router.pup("/edit-message", editMessageFromConversation);
 
 export default router;
